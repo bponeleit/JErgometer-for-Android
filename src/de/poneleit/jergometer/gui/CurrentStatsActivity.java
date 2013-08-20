3442a86dafbb4f2d32e05485dab330358dcff238
Burkart Poneleit <burkartponeleit@Burkarts-MacBook-Air.local>
2013-08-20 21:04:54 +0200

    Leave BikeListener in Jergometer class

--- src/de/poneleit/jergometer/gui/CurrentStatsActivity.java
+++ src/de/poneleit/jergometer/gui/CurrentStatsActivity.java
@@ -1,5 +1,6 @@
 package de.poneleit.jergometer.gui;
 
+import org.jergometer.Jergometer;
 import org.jergometer.communication.BikeListener;
 import org.jergometer.model.DataRecord;
 
@@ -7,37 +8,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import de.poneleit.jergometer.R;
 
-public class CurrentStatsActivity extends Activity implements BikeListener {
+public class CurrentStatsActivity extends Activity {
+	
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.currentstats_activity);
+	    new Jergometer(true, this);
 	}
 
-	@Override
-	public void bikeAck() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void bikeData(DataRecord data) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void bikeError() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void bikeDestPowerChanged(int change) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
