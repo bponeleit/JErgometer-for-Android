d6761416fe0385eef1909074a73d3692ad9439a3
Burkart Poneleit <burkartponeleit@Burkarts-MacBook-Air.local>
2013-09-09 22:17:09 +0200

    Replaced BufferedReader with direct read of driver

--- src/org/jergometer/communication/KettlerBikeReader.java
+++ src/org/jergometer/communication/KettlerBikeReader.java
@@ -2,11 +2,16 @@ package org.jergometer.communication;
 
 import org.jergometer.model.DataRecord;
 
+import android.util.Log;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+
 import de.poneleit.jergometer.R;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 /**
@@ -14,21 +19,30 @@ import java.util.ArrayList;
  */
 public class KettlerBikeReader extends Thread {
 
-// static
+	// static
+	private static final String TAG = KettlerBikeReader.class.getSimpleName();
 
-	public static enum PrintAvailable { none, characters, decimals, hexadecimal }
+	public static enum PrintAvailable {
+		none, characters, decimals, hexadecimal
+	}
 
 	/** Client commands. */
-	public static final String CMD_ACK      = "ACK";
-	public static final String CMD_ERROR    = "ERROR";
-	public static final String CMD_RUN      = "RUN";
+	public static final String CMD_ACK = "ACK";
+	public static final String CMD_ERROR = "ERROR";
+	public static final String CMD_RUN = "RUN";
 
-// dynamic
+	// dynamic
 
 	/** Input stream. */
-	private InputStream inStream;
+	// private InputStream inStream;
 	private boolean closed = false;
-	private BufferedReader in;
+
+	private static final int BUFSIZ = 4096;
+
+	private final UsbSerialDriver mDriver;
+
+	private final ByteBuffer mReadBuffer = ByteBuffer.allocate(BUFSIZ);
+	// private BufferedReader in;
 	/** BikeReaderListeners. */
 	private ArrayList<BikeListener> bikeListeners = new ArrayList<BikeListener>();
 	/** Print available bytes (for debugging). */
@@ -38,39 +52,55 @@ public class KettlerBikeReader extends Thread {
 
 	/**
 	 * Creates the reader for the incoming messages of the bike.
-	 *
-	 * @param in input stream
+	 * 
+	 * @param in
+	 *            input stream
 	 */
-	public KettlerBikeReader(InputStream in) {
-		this.inStream = in;
-		this.in = new BufferedReader(new InputStreamReader(inStream), 1);
+	public KettlerBikeReader(UsbSerialDriver driver) {
+		mDriver = driver;
 	}
 
 	public void run() {
-		while(!isInterrupted()) {
+		while (!isInterrupted()) {
 			try {
-				String dataString = in.readLine();
+				String dataString = null;
+				StringBuffer buf = new StringBuffer();
+				int len = 0;
+				do {
+					mDriver.read(mReadBuffer.array(), 1000);
+					if (len > 0) {
+						Log.d(TAG, "Read data len=" + len);
+						final byte[] data = new byte[len];
+						mReadBuffer.get(data, 0, len);
+						dataString = new String(data);
+						buf.append(dataString);
+						// dataString = new String(mReadBuffer);
+						Log.d(TAG, "dataString: " + dataString);
+						mReadBuffer.clear();
+						// mReadBuffer = new byte[BUFSIZ];
+					}
+				} while (len > 0);
+				if (buf.length() > 0)
+					dataString = buf.toString();
+
 				if (dataString == null || closed) {
 					return;
 				}
 
-				if(printAvailable == PrintAvailable.none) {
+				if (printAvailable == PrintAvailable.none) {
 					if (dataString.contains(CMD_ACK)) {
 						for (BikeListener listener : bikeListeners) {
 							listener.bikeAck();
 						}
-					}
-					else if (dataString.equals(CMD_ERROR)) {
+					} else if (dataString.equals(CMD_ERROR)) {
 						for (BikeListener listener : bikeListeners) {
 							listener.bikeError();
 						}
-					}
-					else if (dataString.equals(CMD_RUN)) {
+					} else if (dataString.equals(CMD_RUN)) {
 						for (BikeListener listener : bikeListeners) {
 							listener.bikeAck();
 						}
-					}
-					else {
+					} else {
 						DataRecord data = new DataRecord(dataString);
 
 						if (lastJergometerDesPower == 0) {
@@ -78,27 +108,26 @@ public class KettlerBikeReader extends Thread {
 						}
 
 						for (BikeListener listener : bikeListeners) {
-							if (data.getDestPower() != lastJergometerDesPower && data.getDestPower() != jergometerDestPower) {
-								listener.bikeDestPowerChanged((data.getDestPower() - jergometerDestPower)/5);
+							if (data.getDestPower() != lastJergometerDesPower
+									&& data.getDestPower() != jergometerDestPower) {
+								listener.bikeDestPowerChanged((data
+										.getDestPower() - jergometerDestPower) / 5);
 							}
 							listener.bikeData(data);
 						}
 						lastJergometerDesPower = data.getDestPower();
 					}
-				}
-				else {
+				} else {
 					// for debugging
 					byte[] bytes = dataString.getBytes();
 
-					if(printAvailable == PrintAvailable.characters) {
+					if (printAvailable == PrintAvailable.characters) {
 						System.err.print(dataString);
-					}
-					else if(printAvailable == PrintAvailable.decimals) {
+					} else if (printAvailable == PrintAvailable.decimals) {
 						for (byte aByte : bytes) {
 							System.out.print("," + (aByte & 0xFF));
 						}
-					}
-					else if(printAvailable == PrintAvailable.hexadecimal) {
+					} else if (printAvailable == PrintAvailable.hexadecimal) {
 						for (byte aByte : bytes) {
 							System.out.format(",%X", (aByte & 0xFF));
 						}
@@ -107,15 +136,13 @@ public class KettlerBikeReader extends Thread {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			yield();
 		}
 	}
 
 	public void close() {
 		closed = true;
-		try {
-			inStream.close();
-			this.interrupt();
-		} catch (IOException ignored) {}
+		this.interrupt();
 		super.interrupt();
 	}
 
