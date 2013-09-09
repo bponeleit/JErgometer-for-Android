5cd74848ce80f3bb53be76b87305b0f80816e140
Burkart Poneleit <burkartponeleit@Burkarts-MacBook-Air.local>
2013-09-09 22:15:30 +0200

    added debug output

--- src/org/jergometer/communication/KettlerBikeConnector.java
+++ src/org/jergometer/communication/KettlerBikeConnector.java
@@ -1,6 +1,7 @@
 package org.jergometer.communication;
 
 import java.io.IOException;
+import android.app.Activity;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -11,6 +12,8 @@ import org.jergometer.translation.I18n;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
@@ -21,17 +24,21 @@ import de.poneleit.jergometer.R;
  * USB). It is used to receive data from the bike and to control it.
  */
 public class KettlerBikeConnector implements BikeConnector {
+	
+	private static final String TAG = KettlerBikeConnector.class.getSimpleName();
 
 	// dynamic
 
-	private Context context = null;
+	public Context context = null;
 	private UsbSerialDriver mDriver = null;
 	public KettlerBikeReader reader = null;
 	public KettlerBikeWriter writer = null;
 	private int power;
 
 	public void connect(String serialName, BikeListener listener) throws BikeException, IOException {
+		Log.d(TAG, "Connect to " + serialName + "...");
 		// Get UsbManager from Android.
+		TextView mDumpTextView = (TextView)((Activity)context).findViewById(R.id.textView2);
 		UsbManager manager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
 		List<UsbSerialDriver> mDrivers = null;
 		Map<String, UsbDevice> devices = manager.getDeviceList();
@@ -45,34 +52,49 @@ public class KettlerBikeConnector implements BikeConnector {
 				mDriver = mDrivers.get(0);
 				connect(mDriver);
 			} catch (IndexOutOfBoundsException e) {
+				
 				// TODO: handle exception
-				//mDumpTextView.append(e.toString() + "\n");
+				Log.e(TAG, "No in list driver found", e);
+				mDumpTextView.setText(e.toString() + "\n");
+				throw new IOException(e);
 			}
 		} else {
-			//mDumpTextView.append("No drivers found\n");
+			Log.e(TAG, "No drivers found");
+			mDumpTextView.setText("No drivers found\n");
+			throw new IOException("No drivers found!");
 		}
+//		mDriver.open();
 		reader.addBikeReaderListener(listener);
+//		((Activity)context).runOnUiThread(reader);
 		reader.start();
 	}
 
 	public void connect(UsbSerialDriver serialPort) throws  IOException {
+		Log.d(TAG, "connect serialPort " + serialPort.toString());
+		serialPort.open();
 		serialPort.setParameters(9600, UsbSerialDriver.DATABITS_8,
 				UsbSerialDriver.STOPBITS_1, UsbSerialDriver.PARITY_NONE);
 
 		// set reader and writer
 		UsbSerialOutputStream usbWriter = new UsbSerialOutputStream(serialPort);
 		writer = new KettlerBikeWriter(true, usbWriter);
-		UsbSerialInputStream usbReader = new UsbSerialInputStream(serialPort);
-		reader = new KettlerBikeReader(usbReader);
+//		UsbSerialInputStream usbReader = new UsbSerialInputStream(serialPort);
+		reader = new KettlerBikeReader(serialPort);
 	}
 
 	@Override
 	public void sendHello() throws IOException {
+//		if (!reader.isAlive()) {
+//			reader.start();
+//		}
 		writer.sendHello();
 	}
 
 	@Override
 	public void sendReset() throws IOException {
+//		if (!reader.isAlive()) {
+//			reader.start();
+//		}
 		writer.sendReset();
 	}
 
